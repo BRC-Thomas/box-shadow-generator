5e34f56e611f5d6aa8bc570d6c11939c777f618d
Thomas <thomasbaracani@gmail.com>
2023-09-16 14:12:04 +0200

    Shadow list & addShadow

--- src/layouts/ShadowList/ShadowList.jsx
+++ src/layouts/ShadowList/ShadowList.jsx
@@ -1,5 +1,29 @@
+import { useSelector, useDispatch } from "react-redux"
+import {addShadow} from "../../features/shadows"
+import Shadow from "./Shadow"
+
 export default function ShadowList() {
+  const dispatch = useDispatch()
+  const shadows = useSelector(state => state.shadows)
+
   return (
-    <div>ShadowList</div>
+    <div className="">
+      <div className="flex justify-between p-6 border-b border-gray-300">
+        <p className="font-bold text-lg">Customize Shadows</p>
+        <button 
+          onClick={() => dispatch(addShadow())}
+          className="py-1 px-3 text-sm rounded bg-blue-600 focus:outline-none focus:ring-4 focus:ring-offset-1 hover:bg-blue-700 text-white">Add a shadow
+        </button>
+      </div>
+      <ul>
+        {shadows.map((shadow, index) => (
+          <Shadow 
+            key={shadow.id}
+            panelNumber={index + 1}
+            shadow={shadow}
+          />
+        ))}
+      </ul>
+    </div>
   )
 }
