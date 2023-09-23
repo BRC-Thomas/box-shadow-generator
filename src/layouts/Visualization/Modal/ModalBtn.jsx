165f93808dafb588162edf93f9cc307afa1c4d12
Thomas <thomasbaracani@gmail.com>
2023-09-23 19:36:05 +0200

    State value to the box

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,5 +1,5 @@
 export default function ModalBtn() {
   return (
-    <div className="mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700">Get the code</div>
+    <div className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white cursor-pointer hover:bg-blue-700">Get the code</div>
   )
 }
