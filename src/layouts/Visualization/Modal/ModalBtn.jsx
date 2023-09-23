1962cc523f7e9f13914af52e8c029edbe0acddc0
Thomas <thomasbaracani@gmail.com>
2023-09-23 20:14:35 +0200

    Modal get code + remove

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import ModalResult from "./ModalResult"
+
 export default function ModalBtn() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white cursor-pointer hover:bg-blue-700">Get the code</div>
+    <>
+      <div 
+      onClick={()=>setShowModal(!showModal)}
+      className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600   text-white cursor-pointer hover:bg-blue-700">
+        Get the code
+      </div>
+      {showModal && 
+        createPortal(<ModalResult closeModal={()=> setShowModal(!showModal)}/>,document.body
+      )}
+    </>
   )
 }
