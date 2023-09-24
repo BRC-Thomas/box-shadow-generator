6ef68cc419bd7fc765e6a3204abe50a3d7d35b72
Thomas <thomasbaracani@gmail.com>
2023-09-24 10:37:25 +0200

    🚀gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  homepage: "https://brc-thomas.github.io/box-shadow-generator",
   plugins: [react()],
 })
